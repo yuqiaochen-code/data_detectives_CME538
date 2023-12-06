daa2ef8c491c7544646d7924c0f6a281b591ae7d
Hongcheng Guo <144384769+AlexGuo0535@users.noreply.github.com>
2023-12-05 20:37:59 -0500

    Bike_lanes_Analysis.py

--- code/Lanes_Coliisions.py
+++ code/Lanes_Coliisions.py
@@ -1,66 +1,73 @@
 import geopandas as gpd
 from shapely.wkt import loads
 from shapely.geometry import Point, LineString, MultiLineString
-from sklearn.neighbors import BallTree
+from sklearn.neighbors import BallTree, _kd_tree
 import pandas as pd
+import folium
+from folium.plugins import MarkerCluster, HeatMap
 
-# Load bike lanes and collisions data
-bike_lanes = pd.read_csv('bikelanes_file.csv')
-bike_lanes_gdf = gpd.GeoDataFrame(
-    bike_lanes.loc[:, [c for c in bike_lanes.columns if c != "geometry"]],
-    geometry=gpd.GeoSeries.from_wkt(bike_lanes["geometry"]),
-    crs="epsg:4326",
-)
+def load_and_clean_bike_lanes(file_path):
+    bike_lanes = pd.read_csv(file_path)
+    bike_lanes_gdf = gpd.GeoDataFrame(
+        bike_lanes.loc[:, [c for c in bike_lanes.columns if c != "geometry"]],
+        geometry=gpd.GeoSeries.from_wkt(bike_lanes["geometry"]),
+        crs="epsg:4326",
+    )
 
-collisions = pd.read_csv('Cyclists.csv')
-collisions = collisions[collisions['YEAR'] >= 2018]
+    # Data cleaning
+    bike_lanes_gdf['geometry_coordinates'] = bike_lanes_gdf['geometry'].apply(get_coordinates)
+    bike_lanes_gdf_cleaned = bike_lanes_gdf.dropna(subset=['geometry_coordinates'])
 
-# Data cleaning
-# bike_lanes_gdf['name'] = bike_lanes_gdf['name'].str.extract(r'([a-zA-Z\s]+)', expand=False).str.strip()
-# collisions['STREET1'] = collisions['STREET1'].str.extract(r'([a-zA-Z\s]+)', expand=False).str.strip()
-# collisions['STREET2'] = collisions['STREET2'].str.extract(r'([a-zA-Z\s]+)', expand=False).str.strip()
+    bike_lanes_gdf_cleaned.to_csv('bike_lanes_gdf_cleaned.csv', index=False)
 
-# Ensure all geometries in bike_lanes_gdf are Lines or MultiLines
-def get_coordinates(geom):
-    if isinstance(geom, LineString) or isinstance(geom, MultiLineString) or isinstance(geom, Point):
-        centroid = geom.centroid
-        return (centroid.x, centroid.y)
-    else:
-        return (0, 0)
+    return bike_lanes_gdf_cleaned
 
+def load_and_clean_collisions(file_path):
+    collisions = pd.read_csv(file_path)
+    collisions = collisions[collisions['YEAR'] >= 2018]
 
-# Clean rows containing NaN in geometry_coordinates
-bike_lanes_gdf['geometry_coordinates'] = bike_lanes_gdf['geometry'].apply(get_coordinates)
-bike_lanes_gdf_cleaned = bike_lanes_gdf.dropna(subset=['geometry_coordinates'])
+    geometry_collisions = [Point(xy) for xy in zip(collisions['LONGITUDE'], collisions['LATITUDE'])]
+    gdf_collisions = gpd.GeoDataFrame(collisions, geometry=geometry_collisions, crs="EPSG:4326")
+    gdf_collisions['geometry_coordinates'] = gdf_collisions['geometry'].apply(get_coordinates)
 
-bike_lanes_gdf_cleaned.to_csv('bike_lanes_gdf_cleaned.csv', index=False)
+    gdf_collisions.to_csv('gdf_collisions_cleaned.csv', index=False)
 
-# Create GeoDataFrame for collisions
-geometry_collisions = [Point(xy) for xy in zip(collisions['LONGITUDE'], collisions['LATITUDE'])]
-gdf_collisions = gpd.GeoDataFrame(collisions, geometry=geometry_collisions, crs="EPSG:4326")
-gdf_collisions['geometry_coordinates'] = gdf_collisions['geometry'].apply(get_coordinates)
+    return gdf_collisions
 
-gdf_collisions.to_csv('gdf_collisions_cleaned.csv', index=False)
+def find_nearest_bike_lanes(bike_lanes_gdf_cleaned, gdf_collisions):
+    tree = BallTree(bike_lanes_gdf_cleaned['geometry_coordinates'].tolist(), metric='haversine')
 
-# Create BallTree for bike lanes
-tree = BallTree(bike_lanes_gdf_cleaned['geometry_coordinates'].tolist(), metric='haversine')
+    distances, indices = tree.query(gdf_collisions['geometry_coordinates'].tolist(), k=1)
 
-# Find the distance to the nearest bike lane for each collision
-distances, indices = tree.query(gdf_collisions['geometry_coordinates'].tolist(), k=1)
-# gdf_collisions['nearest_bike_lane_code'] = indices
+    nearest_bike_lane_info = bike_lanes_gdf_cleaned.iloc[indices.flatten()]
 
-# Get the corresponding bike lane information
-nearest_bike_lane_info = bike_lanes_gdf_cleaned.iloc[indices.flatten()]
+    collisions_with_distances = pd.concat([gdf_collisions, nearest_bike_lane_info[['code', 'name']]], axis=1)
 
-# Reset the index of both dataframes
-collisions.reset_index(drop=True, inplace=True)
-nearest_bike_lane_info.reset_index(drop=True, inplace=True)
+    collisions_with_distances.to_csv('collisions_with_distances.csv', index=False)
 
-# Merge the information back to the original collisions DataFrame
-collisions_with_distances = pd.concat([collisions, nearest_bike_lane_info[['code', 'name']]], axis=1)
+    return collisions_with_distances[['ACCNUM', 'name', 'code']]
 
-# Save the merged data to a new CSV file
-collisions_with_distances.to_csv('collisions_with_distances.csv', index=False)
+def create_toronto_map(collisions, map_type='marker'):
+    toronto_map = folium.Map(location=[43.70, -79.42], zoom_start=11)
 
-collisions_and_bikelanes = collisions_with_distances[['ACCNUM', 'name', 'code']]
-collisions_and_bikelanes.to_csv('collisions_and_bikelanes.csv', index=False)
+    if map_type == 'heatmap':
+        heat_data = [[row['LATITUDE'], row['LONGITUDE']] for _, row in collisions.iterrows()]
+        HeatMap(heat_data, radius=15, blur=10, max_zoom=13).add_to(toronto_map)
+        toronto_map.save('toronto_collisions_heatmap.html')
+    elif map_type == 'marker':
+        marker_cluster = MarkerCluster().add_to(toronto_map)
+        for _, row in collisions.iterrows():
+            folium.Marker([row['LATITUDE'], row['LONGITUDE']], popup=f"Collision {row['ACCNUM']}").add_to(marker_cluster)
+        toronto_map.save('toronto_collisions_marker_map.html')
+
+# Load and clean bike lanes data
+bike_lanes_gdf_cleaned = load_and_clean_bike_lanes('bikelanes_file.csv')
+
+# Load and clean collisions data
+gdf_collisions = load_and_clean_collisions('Cyclists.csv')
+
+# Find nearest bike lanes for each collision
+collisions_and_bikelanes = find_nearest_bike_lanes(bike_lanes_gdf_cleaned, gdf_collisions)
+
+# Create Toronto map with either heatmap or marker cluster
+create_toronto_map(collisions_and_bikelanes, map_type='marker')
